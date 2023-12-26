3989cb51b2fb3821d5f7b55f9eecd581b464c572
Lucas Lara <lucaslara99@gmail.com>
2023-12-26 13:54:52 +0100

    First test approach

--- jjoo-frontend/src/App.test.tsx
+++ jjoo-frontend/src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Consulta and CRUD de SedeJJOO tabs', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  // Comprobar que se muestran las dos pestañas
+  const consultaTab = screen.getByText(/Consulta/i);
+  const crudTab = screen.getByText(/CRUD de SedeJJOO/i);
+  expect(consultaTab).toBeInTheDocument();
+  expect(crudTab).toBeInTheDocument();
+
+  // Comprobar que la pestaña Consulta está seleccionada por defecto
+  expect(consultaTab).toHaveAttribute('aria-selected', 'true');
+  expect(crudTab).toHaveAttribute('aria-selected', 'false');
+
+  // Simular un click en la pestaña CRUD de SedeJJOO y comprobar que se selecciona
+  fireEvent.click(crudTab);
+  expect(crudTab).toHaveAttribute('aria-selected', 'true');
+  expect(consultaTab).toHaveAttribute('aria-selected', 'false');
 });
