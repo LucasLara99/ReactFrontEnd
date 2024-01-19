48725a4d4b1d808084b1f6148fdc740d865f99d1
Lucas Lara <lucaslara99@gmail.com>
2024-01-19 13:35:34 +0100

    Consulta test finished

--- jjoo-frontend/src/components/Consulta/Consulta.test.tsx
+++ jjoo-frontend/src/components/Consulta/Consulta.test.tsx
@@ -1,6 +1,6 @@
 // Consulta.test.tsx
 import React from 'react';
-import { waitFor } from '@testing-library/react';
+import { waitFor, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Consulta from './Consulta';
 import fetchMock from 'fetch-mock';
@@ -8,60 +8,60 @@ import renderWithProviders from './test-utils';
 import { ErrorContext } from '../../hooks/ErrorContext';
 
 describe('Componente Consulta', () => {
-  test('Maneja los errores correctamente', async () => {
-    // Simula un error en la respuesta de la API
-    fetchMock.get('http://localhost:8080/consulta', () => {
-      throw new Error('Error de red');
-    });
+	test('Maneja los errores correctamente', async () => {
+		// Simula un error en la respuesta de la API
+		fetchMock.get('http://localhost:8080/consulta', () => {
+			throw new Error('Error de red');
+		});
 
-    const mockAddError = jest.fn();
+		const mockAddError = jest.fn();
 
-    const TestComponent = () => {
-      return (
-        <ErrorContext.Provider value={{ errors: {}, addError: mockAddError, removeError: () => { }, setErrors: () => { } }}>
-          <Consulta />
-        </ErrorContext.Provider>
-      );
-    };
+		const TestComponent = () => {
+			return (
+				<ErrorContext.Provider value={{ errors: {}, addError: mockAddError, removeError: () => { }, setErrors: () => { } }}>
+					<Consulta />
+				</ErrorContext.Provider>
+			);
+		};
 
-    renderWithProviders(<TestComponent />, null);
+		renderWithProviders(<TestComponent />, null);
 
-    await waitFor(() => {
-      expect(mockAddError).toHaveBeenCalled();
-    });
+		await waitFor(() => {
+			expect(mockAddError).toHaveBeenCalled()
+		});
 
-    fetchMock.restore();
-  });
+		fetchMock.restore();
+	});
 
-  // test('Renderiza la tabla cuando los datos se han cargado', async () => {
-  //   // Simula la respuesta de la API
-  //   fetchMock.get('http://localhost:8080/consulta',
-  //     [
-  //       {
-  //         id_pais: 1,
-  //         nombre_pais: 'Test Country',
-  //         id_ciudad: 1,
-  //         nombre_ciudad: 'Test City',
-  //         valor: 100,
-  //         descripcion_tipo_jjoo: 'Test Description',
-  //         numero_veces_sede: 1,
-  //       },
-  //     ],
-  //   );
+	test('Renderiza la tabla cuando los datos se han cargado', async () => {
+		// Simula la respuesta de la API
+		fetchMock.get('http://localhost:8080/consulta',
+			[
+				{
+					id_pais: 1,
+					nombre_pais: 'Test Country',
+					id_ciudad: 1,
+					nombre_ciudad: 'Test City',
+					valor: 100,
+					descripcion_tipo_jjoo: 'Test Description',
+					numero_veces_sede: 1,
+				},
+			],
+		);
 
-  //   // Envuelve el componente en un QueryClientProvider y ConsultaContext.Provider
-  //   renderWithProviders(<Consulta />, null);
+		// Envuelve el componente en un QueryClientProvider y ConsultaContext.Provider
+		renderWithProviders(<Consulta />, null);
 
-  //   await waitFor(() => {
-  //     expect(screen.getByText('Test Country')).toBeInTheDocument();
-  //     expect(screen.getByText('Test City')).toBeInTheDocument();
-  //     expect(screen.getByText('Test Description')).toBeInTheDocument();
-  //   });
+		await waitFor(() => {
+			expect(screen.getByText('Test Country')).toBeInTheDocument();
+			expect(screen.getByText('Test City')).toBeInTheDocument();
+			expect(screen.getByText('Test Description')).toBeInTheDocument();
+		});
 
-  //   // Verifica que la API se llamó correctamente
-  //   expect(fetchMock.called('http://localhost:8080/consulta')).toBe(true);
+		// Verifica que la API se llamó correctamente
+		expect(fetchMock.called('http://localhost:8080/consulta')).toBe(true);
 
-  //   // Limpia los mocks después de cada prueba
-  //   fetchMock.restore();
-  // });
+		// Limpia los mocks después de cada prueba
+		fetchMock.restore();
+	});
 });
